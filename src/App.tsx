f65feaee5ba8f8729d5ec2a4649eac97a7121be4
Dylan Lamont <didley.dev@gmail.com>
2023-09-17 22:55:00 +1000

    Adds privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import screenshot1 from './assets/screenshot1.jpg'
 import screenshot2 from './assets/screenshot2.jpg'
 import screenshot3 from './assets/screenshot3.jpg'
+import { privacyPolicy } from './privacyPolicy'
 
 
 function App() {
@@ -13,6 +14,12 @@ function App() {
       <img src={screenshot1} className='img' />
       <img src={screenshot2} className='img' />
       <img src={screenshot3} className='img' />
+      <div>
+        <h2>Privacy Policy</h2>
+        <div style={{ whiteSpace: 'pre-wrap', textAlign: 'left', fontSize: 12 }}>
+          {privacyPolicy}
+        </div>
+      </div>
     </>
   )
 }
